d5d8b35145b792ba8b7aa94828d402a453b44a02
Trevor Otterson <totterson97@gmail.com>
2020-10-04 14:39:53 -0600

    added in transaction tables and comments

--- models/transaction.js
+++ models/transaction.js
@@ -1,16 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
+
 const Schema = mongoose.Schema
 
+// db tables
 const transactionSchema = new Schema(
   {
     name: {
       type: String,
       trim: true,
-      required: "Enter a name for the transaction"
+      required: 'Enter a name for transaction'
     },
     value: {
       type: Number,
-      required: "Enter an amount"
+      required: 'Enter an amount'
     },
     date: {
       type: Date,
@@ -19,5 +21,5 @@ const transactionSchema = new Schema(
   }
 )
 
-const Transaction = mongoose.model("Transaction", transactionSchema)
+const Transaction = mongoose.model('Transaction', transactionSchema)
 module.exports = Transaction
