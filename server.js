5cb292801c78c82ffc261c2503fff1047be715f9
Trevor Otterson <totterson97@gmail.com>
2020-10-07 00:50:56 -0600

    fixing db errors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.static('public'))
 
 // db connection
-mongoose.connect('mongodb://localhost/budget', {
+mongoose.connect('mongodb://localhost/Transaction', {
   useNewUrlParser: true,
   useFindAndModify: false
 })
